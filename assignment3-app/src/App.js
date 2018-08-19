aeb3ca1d023aab51c7f8c199a051af4cee3b8c07
mioyuan <yuan2583@gmail.com>
2018-08-20 01:19:55 +1000

    added gallery and footer

--- assignment3-app/src/App.js
+++ assignment3-app/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Header from './components/header';
 //import Gallery from './components/gallery';
+import Footer from './components/Footer';
+import { Carousel } from 'antd';
+import testimage1 from './images/1.png';
 
 class App extends Component {
   render(){
@@ -8,6 +11,17 @@ class App extends Component {
       <div>
         <Header />
         {/* <Gallery /> */}
+
+        <div className="Gallery">
+          <Carousel effect="fade">
+             <div><img src={testimage1}/></div>
+             <div><img src={testimage1}/></div>
+             <div><img src={testimage1}/></div>
+             <div><img src={testimage1}/></div>
+         </Carousel>
+        </div>
+
+        <Footer/>
       </div>
     );
   }
---
+++ assignment3-app/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Footer.css';
+
+const footer = () => {
+    return (
+        <div className="Footer">
+            <table id="Siteinfo">
+                <tr>
+                    <td>About Us |</td>
+                    <td>Contact |</td>
+                    <td>Terms of Use |</td>
+                    <td>Privacy Policy |</td>
+                </tr>
+            </table>
+            <p id="copyright">© 2018 Peppa Filmtopia. Film data from IMDb. Made by 🐰🦄🐹 with ♥️</p>
+        </div>
+    )
+};
+
+export default footer;
